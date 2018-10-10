6cdb9a160bbdd0478bba82955798d23541c0c912
LINXufeng <felixhui@10.89.87.150>
2018-10-11 01:17:00 +0800

    Make some tiny change

--- src/main/java/comp3111/webscraper/Controller.java
+++ src/main/java/comp3111/webscraper/Controller.java
@@ -69,7 +69,8 @@ public class Controller {
     		output += item.getTitle() + "\t" + item.getPrice() + "\t" + item.getUrl() + "\n";
     	}
     	textAreaConsole.setText(output);
-    	
+    	// add following line
+    	labelCount.setText("Hi");
     	
     }
     
